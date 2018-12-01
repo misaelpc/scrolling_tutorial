5035e61ec53e08ebfd385ca81e38b78b508189c8
Misael Pérez Chamorro <misaelperezchamorro@Misaels-MacBook-Pro.local>
2018-11-30 20:46:38 -0600

    Scroloing

--- PhotoScroll/ZoomedPhotoViewController.swift
+++ PhotoScroll/ZoomedPhotoViewController.swift
@@ -32,6 +32,11 @@ import UIKit
 
 class ZoomedPhotoViewController: UIViewController {
   @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+  @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+  @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+  @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
   var photoName: String?
   
   override func viewDidLoad() {
@@ -40,6 +45,43 @@ class ZoomedPhotoViewController: UIViewController {
     }
   }
   
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    updateMinZoomScaleForSize(view.bounds.size)
+  }
+  
+  func updateMinZoomScaleForSize(_ size: CGSize) {
+    let widthScale = size.width / imageView.bounds.width
+    let heightScale = size.height / imageView.bounds.height
+    let minScale = min(widthScale, heightScale)
+    
+    scrollView.minimumZoomScale = minScale
+    scrollView.zoomScale = minScale
+  }
+  
+}
+
+extension ZoomedPhotoViewController: UIScrollViewDelegate {
+  func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+    return imageView
+  }
+  
+  func scrollViewDidZoom(_ scrollView: UIScrollView) {
+    updateConstraintsForSize(view.bounds.size)
+  }
+  
+  func updateConstraintsForSize(_ size: CGSize) {
+    
+    let yOffset = max(0, (size.height - imageView.frame.height) / 2)
+    imageViewTopConstraint.constant = yOffset
+    imageViewBottomConstraint.constant = yOffset
+    
+    let xOffset = max(0, (size.width - imageView.frame.width) / 2)
+    imageViewLeadingConstraint.constant = xOffset
+    imageViewTrailingConstraint.constant = xOffset
+    
+    view.layoutIfNeeded()
+  }
 }
 
 
